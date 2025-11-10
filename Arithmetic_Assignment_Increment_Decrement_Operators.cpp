df9556099bf79e12d18dcc1404bf4c3be1ce13fa
Ayush Mathur <160166699+Ayush056github@users.noreply.github.com>
2025-11-10 22:12:20 +0530

    Add arithmetic operations and increment/decrement examples
    Implement basic arithmetic and increment/decrement operations.

---
+++ Arithmetic_Assignment_Increment_Decrement_Operators.cpp
@@ -0,0 +1,23 @@
+//
+
+#include<iostream>
+
+using namespace std;
+
+main()
+{
+    int a=10,b=5;
+
+    cout << a + b <<endl; 
+    cout << a - b <<endl; 
+    cout << a / b <<endl; 
+    cout << a * b <<endl; 
+
+    cout << a % b <<endl; 
+
+    cout << ++a <<endl; 
+    cout << --a <<endl; 
+    cout << a++ <<endl; 
+    cout << a-- <<endl; 
+
+}
